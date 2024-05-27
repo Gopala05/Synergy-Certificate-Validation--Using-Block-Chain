aa12040bd41634caf7a2389916ced89ccef821e6
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-05-27 19:57:52 +0530

    Formatting

--- Components/Logo/Logo.jsx
+++ Components/Logo/Logo.jsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import React from "react";
 
-import style from './Logo.module.css'
+import style from "./Logo.module.css";
 
 const Logo = () => {
-  return <div className={style.spinner}>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>;
+  return (
+    <div className={style.spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Logo;
