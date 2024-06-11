614c5461c5704aeb347cd2e89ec6005060900526
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-11 11:35:10 +0530

    Auth Controller

--- Api/Controllers/authController.js
+++ Api/Controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const UserModal = require("../Model/userModel");
+const authModal = require("../Model/authModal");
 
 const signInToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
@@ -8,8 +8,8 @@ const signInToken = (id) => {
 };
 
 // Creating a function to generate a Token
-const createToken = (user, status, req, res) => {
-  const token = signInToken(user._id);
+const createToken = (auth, status, req, res) => {
+  const token = signInToken(auth._id);
 
   res.cookie("jwt", token, {
     expires: new Date(
@@ -20,50 +20,53 @@ const createToken = (user, status, req, res) => {
   });
 
   // Remove password from output
-  user.password = undefined;
+  auth.password = undefined;
 
   res.status(status).json({
     status: "Success",
     token,
     data: {
-      user,
+      auth,
     },
   });
 };
 
 // Create the "SignUp" method
 exports.SignUp = async (req, res, next) => {
-  const newUser = await UserModal.create({
-    name: req.body.name,
-    email: req.body.email,
+  const newAuth = await authModal.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    authID: req.body.authID,
+    authEmail: req.body.authEmail,
+    role: req.body.role,
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
   });
 
-  createToken(newUser, 201, req, res);
+  createToken(newAuth, 201, req, res);
 };
 
 exports.SignIn = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { authID, password } = req.body;
 
-  // Check if email and password are passed from user
-  if (!email || !password) {
+  // Check if authID and password are passed from auth
+  if (!authID || !password) {
     res.status(400).json({
       status: "Bad Request",
-      message: "Pleasen provide the email and password!",
+      message: "Pleasen provide the user ID and password!",
     });
   }
-  // Checking if the user exists or not
-  let user = await UserModal.findOne({ email: email }).select("+password");
+  // Checking if the auth exists or not
+  let auth = await authModal.findOne({ authID: authID }).select("+password");
 
-  // Check if user exists and Password and user.password are valid
-  if (!user || !(await user.validatePassword(password, user.password))) {
+  // Check if auth exists and Password and auth.password are valid
+  if (!auth || !(await auth.validatePassword(password, auth.password))) {
     return res.status(401).json({
       status: "Unauthorized",
-      message: "Invalid Email or Password.",
+      message: "Invalid user ID or Password.",
     });
   }
 
   // Check if everything is OK, send Token to the Client
-  createToken(user, 200, req, res);
+  createToken(auth, 200, req, res);
 };
