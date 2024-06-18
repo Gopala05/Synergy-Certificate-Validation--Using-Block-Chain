60b2d989e820b16259de7126ef1c482408fe0b56
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-18 21:18:47 +0530

    Env configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    THIRDWEB_CLIENT_ID: process.env.THIRDWEB_CLIENT_ID,
+    THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY,
+    THIRDWEB_DEPLOYEMENT_ADDRESS: process.env.THIRDWEB_DEPLOYEMENT_ADDRESS,
+    METAMASK_PRIVATE_KEY: process.env.METAMASK_PRIVATE_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
