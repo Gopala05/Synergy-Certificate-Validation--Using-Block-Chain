cfc6835cc7630a4b343fe5eb0e8b135a1b7ccd11
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-20 17:25:22 +0530

    Clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,25 @@
 import "../styles/globals.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { StateContextProvider } from "../Context/NFTs";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThirdwebProvider activeChain={ChainId.Mumbai}>
-      <StateContextProvider>
-        <Component {...pageProps} />
-      </StateContextProvider>
-    </ThirdwebProvider>
+    <ClerkProvider>
+      <ThirdwebProvider activeChain={ChainId.Mumbai}>
+        <StateContextProvider>
+          <Head>
+            <title>Synergy</title>
+            <meta
+              name="description"
+              content="Certificate Validation System by Team SYNERGY"
+            />
+            <link rel="icon" href="/Logo.png" />
+          </Head>
+          <Component {...pageProps} />
+        </StateContextProvider>
+      </ThirdwebProvider>
+    </ClerkProvider>
   );
 }
