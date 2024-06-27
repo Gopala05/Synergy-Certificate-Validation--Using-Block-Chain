3021263197da52ceb23cf1ef263b75f734e15492
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-27 23:33:00 +0530

    Removed midddleware.ts

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/landing", "/auth-login", "/user-dashboard", "/validation"],
-// };
