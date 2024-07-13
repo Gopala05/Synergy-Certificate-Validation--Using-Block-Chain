2c266664b30d0aac6d7a5796500596bbd8252ba1
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-07-13 14:25:27 +0530

    Valid Page Update

--- pages/valid/index.js
+++ pages/valid/index.js
@@ -1,33 +1,190 @@
+"use client";
 import React from "react";
-import DashNav from "../../Components/Nav/DashNav";
-import { Col, Row } from "antd";
+import DashNav from "../../components/nav/DashNav";
+import { Button, Col, Row } from "antd";
+import Footer from "../../Components/Footer/Footer";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { Logo } from "../../Components";
 
-const Valid = ({ info }) => {
+const Valid = () => {
+  const [info, setInfo] = React.useState("");
+  const router = useRouter();
+  const toastShownRef = React.useRef(false);
+
+  React.useEffect(() => {
+    const NFTdata = localStorage.getItem("NFT");
+    if (!NFTdata) {
+      router.replace("/validation");
+      if (!toastShownRef.current) {
+        toast("Verify the Certificate", {
+          icon: "🚫",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+        toastShownRef.current = true;
+      }
+    } else {
+      setInfo(JSON.parse(NFTdata));
+    }
+  }, [router]);
+
+  if (!info) {
+    return (
+      <div className="loader">
+        <Logo />
+      </div>
+    );
+  }
+
+  const handleBack = () => {
+    try {
+      localStorage.removeItem("NFT");
+      router.push("/validation");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Internal Server Error");
+      console.error("Error in Login: ", error);
+    }
+  };
+
   return (
-    <div className="h-[100vh]">
+    <>
       <DashNav />
-      <Row className="flex h-full w-full items-center py-20">
+      <Row className="flex w-full h-full items-center">
         <Col
-          lg={12}
-          className="flex justify-center items-center flex-col gap-y-10 lg:pl-16"
+          lg={13}
+          className="flex items-center h-full lg:h-[93vh] justify-center flex-col gap-y-10 lg:pl-16 pt-10"
         >
-          <div className="border-dashed border-4 border-[#0080DC] bg-white/10  p-5 rounded-3xl">
+          <div className="flex justify-start w-full ">
+            <Button onClick={handleBack} type="primary">
+              Back
+            </Button>
+          </div>
+
+          <div className="border-dashed -z-20 border-2 border-[#0080DC] bg-white/10 p-5 rounded-3xl">
             <img
-              //   src={info.url}
-              src="https://gateway.pinata.cloud/ipfs/QmPFX39GWeasHPzDGcqBpcbiHs4ULgR6X9fk2N37h7FqU3"
+              src={info?.certificate}
               alt="Certificate Image"
-              className=""
+              className="max-w-[35vw]"
             />
           </div>
-          <a download href="https://gateway.pinata.cloud/ipfs/QmPFX39GWeasHPzDGcqBpcbiHs4ULgR6X9fk2N37h7FqU3" >
-            <button className="rounded-full text-2xl font-semibold text-white bg-[#0080DC] py-4 px-8 border-2 border-[#15C586]">
-                Download Certificate
+          <div className="flex w-full gap-x-5 justify-center items-center">
+            <button className="rounded-full text-2xl font-semibold hover:border-white hover:bg-[#15C586] hover:text-black text-white bg-[#0080DC] py-4 px-8 border-2 border-[#15C586]">
+              <a
+                download
+                href={info?.certificate}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-black "
+              >
+                <span>Download Certificate</span>
+              </a>
             </button>
-          </a>
+            <a
+              download
+              href={info?.certificate}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <img
+                src="./Download_Icon.png"
+                alt="Download Icon"
+                className="w-14 pt-2"
+              />
+            </a>
+          </div>
         </Col>
-        <Col lg={12}></Col>
+        <Col
+          lg={11}
+          className="flex flex-col gap-y-10 w-full justify-center items-center pt-24"
+        >
+          {/* Title */}
+          <Row className="flex w-full justify-start gap-5 items-center">
+            <Col
+              lg={5}
+              className="flex justify-end text-end text-white text-2xl font-semibold"
+            >
+              Title:
+            </Col>
+            <Col lg={12}>
+              <Row className="flex w-full bg-white items-center rounded-2xl py-2">
+                <Col className="flex flex-grow pl-5 items-center">
+                  <div className="border-none bg-white text-black tex text-2xl flex items-center font-bold cursor-pointer">
+                    {info?.title}
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          {/* Certificate ID */}
+          <Row className="flex w-full justify-start gap-5 items-center">
+            <Col
+              lg={5}
+              className="flex justify-end text-end text-white text-2xl font-semibold"
+            >
+              Certificate ID:
+            </Col>
+            <Col lg={12}>
+              <Row className="flex w-full bg-white items-center rounded-2xl py-2">
+                <Col className="flex flex-grow pl-5 items-center">
+                  <div className="border-none bg-white text-black tex text-2xl flex items-center font-bold cursor-pointer">
+                    {info?.certificateID}
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          {/* User Email */}
+          <Row className="flex w-full justify-start gap-5 items-center">
+            <Col
+              lg={5}
+              className="flex justify-end text-end text-white text-2xl font-semibold"
+            >
+              Email ID:
+            </Col>
+            <Col lg={12}>
+              <Row className="flex w-full bg-white items-center rounded-2xl py-2">
+                <Col className="flex flex-grow pl-5 items-center">
+                  <div className="border-none bg-white text-black tex text-2xl flex items-center font-bold cursor-pointer">
+                    {info?.userEmail}
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          {/* Org */}
+          <Row className="flex w-full justify-start gap-5 items-center mb-24">
+            <Col
+              lg={5}
+              className="flex justify-end text-end text-white text-2xl font-semibold"
+            >
+              Organization:
+            </Col>
+            <Col lg={12}>
+              <Row className="flex w-full bg-white items-center rounded-2xl py-2">
+                <Col className="flex flex-grow pl-5 items-center">
+                  <div className="border-none bg-white text-black tex text-2xl flex items-center font-bold cursor-pointer">
+                    {info?.organisation}
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Row className="flex w-full">
+          <Col lg={24}>
+            <Footer />
+          </Col>
+        </Row>
       </Row>
-    </div>
+    </>
   );
 };
 
