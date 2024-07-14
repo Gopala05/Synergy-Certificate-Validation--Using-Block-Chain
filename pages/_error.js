c1a15d939b62c5fb31d03366124416d30ab2e38c
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-07-15 02:02:49 +0530

    Error Pages Update

---
+++ pages/_error.js
@@ -0,0 +1,41 @@
+// pages/_error.js
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+
+const Error = () => {
+  return (
+    <div className="h-[100vh] flex flex-col bg-white text-[#073E2A]">
+      <div className="w-full fixed items-baseline z-50 bg-[#02291B]">
+        <div className="bg-white w-full flex px-5 pt-2 pb-0 justify-start items-center z-50">
+          <div className="flex justify-start items-center font-bold text-3xl">
+            <span>
+              <img src="/Logo.png" alt="Logo" className="w-24" />
+            </span>
+            Synergy
+          </div>
+        </div>
+      </div>
+      <div className="flex-grow flex flex-col justify-center  gap-y-4 items-center">
+        <div className="text-5xl font-sans font-bold">
+          Something went wrong. Please try again later.
+        </div>
+        <Link href="/">
+          <p className="text-xl underline font-sans font-bold hover:italic">
+            Go to Home
+          </p>
+        </Link>
+        <img src="/Error_Metamask.png" alt="Error Image" />
+      </div>
+      <footer className="bg-[#02291B] p-3 relative text-center cursor-pointer">
+        <p className="font-semibold text-white m-1 text-xl flex items-center justify-center lg:tracking-wider">
+          <span className="flex items-start text-sm m-0 p-0">©&nbsp;</span>{" "}
+          Copyright 2024 |<span className="ml-1">All Rights Reserved</span> |
+          SYNERGY
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Error;
