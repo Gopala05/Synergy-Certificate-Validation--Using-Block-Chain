5c3c517e08c229c5541d418c6ce710086b512576
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-07-25 22:23:26 +0530

    Responsive Landing Page

--- Components/Blog/Blog.jsx
+++ Components/Blog/Blog.jsx
@@ -5,26 +5,29 @@ import { RiCalendar2Line, RiSearchLine } from "react-icons/ri";
 
 const Blog = () => {
   return (
-    <div id="blog" className="h-[100vh] p-20 flex flex-col justify-center">
+    <div
+      id="blog"
+      className="lg:h-[100vh] p-10 pt-24 lg:p-5 lg:pt-24 xl:p-20 flex flex-col justify-center"
+    >
       <label className="text-5xl font-extrabold flex justify-center">
         See our Blog
       </label>
-      <p className="text-baseline flex justify-center mt-3">
+      <p className="text-baseline flex text-center lg:text-left justify-center mt-3">
         Get the latest news, updates, expert insights, and useful tips.
       </p>
-      <div className="flex justify-center items-center mt-10">
+      <div className="hidden lg:flex justify-center items-center mt-10">
         <div className="relative flex justify-center items-center w-full">
           <input
             className="w-[50vw] h-14 rounded-full pr-10 bg-[#02291B] text-start hover:bg-[#02291B] border-2 border-[#22674E] hover:border-[#22674E] placeholder-white placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
             placeholder="Search..."
           />
           <Link href="/user-login">
-            <RiSearchLine className="absolute right-[27rem] text-white font-bold" />
+            <RiSearchLine className="absolute lg:right-[17rem] lg:top-5 xl:right-[27rem] text-white font-bold" />
           </Link>
         </div>
       </div>
       <div>
-        <Row className="flex justify-center mt-20 gap-10 ">
+        <Row className="flex justify-center lg:mt-20 mt-5 gap-10 ">
           <Col lg={6}>
             <div className="bg-[#02291B] rounded-lg hover:shadow-2xl hover:shadow-green-600 transition-transform duration-300 ease-in-out hover:-translate-y-2">
               <img
@@ -39,7 +42,7 @@ const Blog = () => {
                   </span>
                 </div>
                 <div className="mt-5">
-                  <label className="text-xl font-extrabold mt-5">
+                  <label className="xl:text-2xl lg:text-lg font-extrabold mt-5">
                     Internship Certificate
                   </label>
                 </div>
@@ -64,7 +67,7 @@ const Blog = () => {
                   </span>
                 </div>
                 <div className="mt-5">
-                  <label className="text-xl font-extrabold mt-5">
+                  <label className="xl:text-2xl lg:text-lg font-extrabold mt-5">
                     Hackathon Certificate
                   </label>
                 </div>
@@ -89,7 +92,7 @@ const Blog = () => {
                   </span>
                 </div>
                 <div className="mt-5">
-                  <label className="text-xl font-extrabold mt-5">
+                  <label className="xl:text-2xl lg:text-lg font-extrabold mt-5">
                     Metamask Wallet
                   </label>
                 </div>
