387150989cd724f4efbd22929e254ba87bbea2fd
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-08-14 00:55:51 +0530

    User Sign Up Update

--- pages/user-signup/index.js
+++ pages/user-signup/index.js
@@ -1,11 +1,12 @@
 "use client";
-import { Col, Form, Input, Row } from "antd";
+import { Avatar, Col, Form, Input, Row, Upload } from "antd";
 import React from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { RiHome4Fill } from "react-icons/ri";
+import { Edit2Icon, Edit3Icon, UserIcon } from "lucide-react";
 
 const UserSignUp = () => {
   const router = useRouter();
@@ -15,19 +16,32 @@ const UserSignUp = () => {
   const [passErrror, setPassError] = React.useState("");
   const [confPassInvalid, setConfPassInvalid] = React.useState(false);
   const [confPassErrror, setConfPassError] = React.useState("");
+  const [isEmailInValid, setIsEmailInValid] = React.useState(false);
+  const [emailError, setEmailError] = React.useState("");
+  const [firstField, setFirstField] = React.useState("Aadhar Number");
+  const [buttonName, setButtonName] = React.useState("Get OTP");
+  const [aadharInvalid, setAadharInvalid] = React.useState(false);
+  const [aadharError, setAadharError] = React.useState("");
+  const [verified, setVerified] = React.useState(false);
+  const [avatarImage, setAvatarImage] = React.useState("/User_Name.jpg");
+  const [file, setFile] = React.useState();
   const [user, setUser] = React.useState({
-    name: "",
+    aadhar: "",
+    otp: "",
     userName: "",
     userEmails: "",
     password: "",
     confirmPassword: "",
   });
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   // Key Board Event Listener
   const handleEnterKeyPress = (event) => {
     if (event.key === "Enter") {
-      handleSignUp(event);
+      if (!verified && firstField == "OTP") handleOTPVerification();
+      if (!verified && firstField == "Aadhar Number") handleOTP();
+      if (verified) handleSignUp(event);
     }
   };
 
@@ -49,6 +63,22 @@ const UserSignUp = () => {
   // Handling Input
   const handleFormFieldChange = (fieldName, e) => {
     setIsInValid(false);
+    if (fieldName == "aadhar" && e.target.value.length !== 12) {
+      setAadharInvalid(true);
+      setAadharError("Aadhar Should be 12 characters numeric only");
+    }
+    if (fieldName == "aadhar" && e.target.value.length == 12) {
+      setAadharInvalid(false);
+      setAadharError("");
+    }
+    if (fieldName == "userEmails" && !emailRegex.test(e.target.value)) {
+      setIsEmailInValid(true);
+      setEmailError("Provide the proper Email");
+    }
+    if (fieldName == "userEmails" && emailRegex.test(e.target.value)) {
+      setIsEmailInValid(false);
+      setEmailError("");
+    }
     if (fieldName == "password" && !passwordRegex.test(e.target.value)) {
       setPassInvalid(true);
       setPassError(
@@ -77,7 +107,8 @@ const UserSignUp = () => {
 
     try {
       if (
-        user.name === "" ||
+        user.aadhar === "" ||
+        user.otp === "" ||
         user.userName === "" ||
         user.userEmails === "" ||
         user.password === "" ||
@@ -86,6 +117,16 @@ const UserSignUp = () => {
         setIsInValid(true);
         setError("Please Provide the");
         toast.error("Please Fill All Mandatory Fields");
+        return;
+      }
+
+      if (user.aadhar !== 12) {
+        setAadharInvalid(true);
+        setAadharError("Aadhar Should be 12 characters numeric only");
+      }
+
+      if (!emailRegex.test(user.email)) {
+        toast.error("Provide the proper Email");
         return;
       }
 
@@ -140,6 +181,34 @@ const UserSignUp = () => {
     }
   };
 
+  const handleImage = (file) => {
+    const reader = new FileReader();
+    setFile(file);
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setAvatarImage(reader.result);
+    };
+  };
+
+  const handleOTP = async () => {
+    if (!user.aadhar) return toast.error("Plese Provide the aadhar number");
+    // const aadharExists = await checkAadhar(user.aadhar);
+    if (true) {
+      setFirstField("OTP");
+      setButtonName("Verify");
+    }
+  };
+
+  const handleOTPVerification = async () => {
+    if (!user.otp) return toast.error("Plese Provide the OTP");
+    // const otpValid = await checkOTP(user.aadhar, user.otp);
+    if (true) {
+      setFirstField("Aadhar number");
+      setButtonName("Verified");
+      setVerified(true);
+    }
+  };
+
   return (
     <div className="h-full lg:h-[100vh] bg-white">
       <Row className="flex justify-center items-center">
@@ -152,7 +221,7 @@ const UserSignUp = () => {
         </Col>
         <Col
           lg={12}
-          className="p-5 lg:p-7 xl:p-20 pt-5 h-full lg:h-[100vh] w-full justify-start flex flex-col"
+          className="p-5 lg:p-7 xl:p-20 xl:pt-3 pt-5 h-full lg:h-[100vh] w-full justify-start flex flex-col"
         >
           <div className="flex justify-end items-start">
             <button
@@ -171,31 +240,124 @@ const UserSignUp = () => {
               Enter your details to get Registered
             </p>
 
-            {/* Full Name */}
+            {/* Profile */}
+            <Form.Item className="flex justify-center items-center mb-0 mt-4">
+              <div className="relative">
+                <Upload
+                  name="avatar"
+                  showUploadList={false}
+                  beforeUpload={(file) => {
+                    handleImage(file);
+                    return false;
+                  }}
+                >
+                  <Avatar
+                    shape="circle"
+                    icon={<UserIcon />}
+                    size={100}
+                    src={avatarImage}
+                    className="border-2 border-black"
+                  />
+                  <div className="absolute bottom-0 right-0 p-1">
+                    <Edit3Icon className="text-black border-2 border-black bg-white rounded-full p-1 cursor-pointer" />
+                  </div>
+                </Upload>
+              </div>
+            </Form.Item>
+
+            {/* Aadhar */}
+            <div className="flex w-full gap-x-5 items-center">
+              <Form.Item
+                className="w-full"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please provide Aashar Number!",
+                  },
+                ]}
+                validateStatus={
+                  isInValid && !user.aadhar
+                    ? "error"
+                    : aadharInvalid
+                    ? "error"
+                    : ""
+                }
+                help={
+                  isInValid && !user.aadhar ? (
+                    <p className="text-red-600 text-base font-bold">{`${error} Aadhar Number`}</p>
+                  ) : aadharInvalid ? (
+                    <p className="text-red-600 text-base font-bold">
+                      {aadharError}
+                    </p>
+                  ) : null
+                }
+              >
+                <label className="text-black text-xl xl:text-2xl font-bold">
+                  {firstField}
+                  <span className="text-red-600">*</span>
+                </label>
+                <br />
+                <Input
+                  type="number"
+                  disabled={verified}
+                  className="w-full text-black mt-1 h-5 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
+                  placeholder={
+                    firstField == "OTP" ? "OTP..." : "Aadhar Number..."
+                  }
+                  value={firstField == "OTP" ? user.otp : user.aadhar}
+                  onChange={(e) =>
+                    firstField == "OTP"
+                      ? handleFormFieldChange("otp", e)
+                      : handleFormFieldChange("aadhar", e)
+                  }
+                />
+              </Form.Item>
+              <button
+                disabled={verified || user.aadhar.length == 0 || aadharInvalid}
+                onClick={() =>
+                  firstField == "OTP" ? handleOTPVerification() : handleOTP()
+                }
+                className="btn mt-1 disabled:from-gray-500 disabled:to-gray-500 disabled:hover:cursor-not-allowed disabled:text-white bg-gradient-to-r from-green-400 to-green-600 text-xl border-0 text-black hover:text-white rounded-2xl font-bold hover:shadow-green-600"
+              >
+                {buttonName}
+              </button>
+            </div>
+
+            {/* Email ID */}
             <Form.Item
-              className="mt-5"
               rules={[
                 {
                   required: true,
-                  message: "Please provide your Full Name!",
+                  message: "Please provide Email ID!",
                 },
               ]}
-              validateStatus={isInValid && !user.name ? "error" : ""}
+              validateStatus={
+                isInValid && !user.userEmails
+                  ? "error"
+                  : emailError
+                  ? "error"
+                  : ""
+              }
               help={
-                isInValid && !user.name ? (
-                  <p className="text-red-600 text-base font-bold">{`${error} Full Name`}</p>
+                isInValid && !user.userEmails ? (
+                  <p className="text-red-600 text-base font-bold">{`${error} Email ID`}</p>
+                ) : isEmailInValid ? (
+                  <p className="text-red-600 text-base font-bold">
+                    {emailError}
+                  </p>
                 ) : null
               }
             >
               <label className="text-black text-xl xl:text-2xl font-bold">
-                Full Name<span className="text-red-600">*</span>
+                Email<span className="text-red-600">*</span>
               </label>
               <br />
               <Input
+                disabled={!verified}
                 className="w-full text-black mt-1 h-10 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
-                placeholder="Full Name.."
-                value={user.name}
-                onChange={(e) => handleFormFieldChange("name", e)}
+                placeholder="Email..."
+                value={user.userEmails}
+                onChange={(e) => handleFormFieldChange("userEmails", e)}
               />
             </Form.Item>
 
@@ -219,37 +381,11 @@ const UserSignUp = () => {
               </label>
               <br />
               <Input
+                disabled={!verified}
                 className="w-full text-black mt-1 h-10 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
-                placeholder="User Name.."
+                placeholder="User Name..."
                 value={user.userName}
                 onChange={(e) => handleFormFieldChange("userName", e)}
-              />
-            </Form.Item>
-
-            {/* Email ID */}
-            <Form.Item
-              rules={[
-                {
-                  required: true,
-                  message: "Please provide Email ID!",
-                },
-              ]}
-              validateStatus={isInValid && !user.userEmails ? "error" : ""}
-              help={
-                isInValid && !user.userEmails ? (
-                  <p className="text-red-600 text-base font-bold">{`${error} Email ID`}</p>
-                ) : null
-              }
-            >
-              <label className="text-black text-xl xl:text-2xl font-bold">
-                Email<span className="text-red-600">*</span>
-              </label>
-              <br />
-              <Input
-                className="w-full text-black mt-1 h-10 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
-                placeholder="Email.."
-                value={user.userEmails}
-                onChange={(e) => handleFormFieldChange("userEmails", e)}
               />
             </Form.Item>
 
@@ -283,9 +419,10 @@ const UserSignUp = () => {
               </label>
               <br />
               <Input.Password
+                disabled={!verified}
                 type="password"
                 className="w-full mt-1 text-black h-10 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
-                placeholder="Password.."
+                placeholder="Password..."
                 value={user.password}
                 onChange={(e) => handleFormFieldChange("password", e)}
               />
@@ -324,7 +461,7 @@ const UserSignUp = () => {
                 type="password"
                 disabled={user.password ? false : true}
                 className="w-full mt-1 text-black h-10 rounded-2xl pr-10 border-2 border-[#22674E] placeholder:font-bold text-xl p-5 placeholder:items-center items-center"
-                placeholder="Confirm Password.."
+                placeholder="Confirm Password..."
                 value={user.confirmPassword}
                 onChange={(e) => handleFormFieldChange("confirmPassword", e)}
               />
